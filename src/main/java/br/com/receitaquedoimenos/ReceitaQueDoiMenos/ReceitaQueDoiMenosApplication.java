8a524ee95dbdbe0659494dbf86740d29fcbb95e9
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-10-18 21:59:41 -0300

    Initial Commit: Build Spring Project

---
+++ src/main/java/br/com/receitaquedoimenos/ReceitaQueDoiMenos/ReceitaQueDoiMenosApplication.java
@@ -0,0 +1,13 @@
+package br.com.receitaquedoimenos.ReceitaQueDoiMenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitaQueDoiMenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitaQueDoiMenosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/receitaquedoimenos/ReceitaQueDoiMenos/ReceitaQueDoiMenosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.receitaquedoimenos.ReceitaQueDoiMenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaQueDoiMenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
