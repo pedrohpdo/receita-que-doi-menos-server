2f6e3499d3a10ee9b9cea4762ac212df3f7f892c
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-11-09 12:17:33 -0300

    feat: insert creator data on meals/drinks responses

---
+++ src/main/java/br/com/receitaquedoimenos/ReceitaQueDoiMenos/models/user/UserCreatorRecipeResponseDTO.java
@@ -0,0 +1,8 @@
+package br.com.receitaquedoimenos.ReceitaQueDoiMenos.models.user;
+
+public record UserCreatorRecipeResponseDTO(
+        String id,
+        String name,
+        String email
+) {
+}
