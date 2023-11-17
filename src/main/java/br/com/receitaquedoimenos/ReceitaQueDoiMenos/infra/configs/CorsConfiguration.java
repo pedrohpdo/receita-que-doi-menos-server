40c5b28ca4409c3559e5f48a54e4ff435d088d18
Pedro Henrique Pereira <pedropereira2312@gmail.com>
2023-11-16 21:05:36 -0300

    fix(cors): update cors restrictions from origin

--- src/main/java/br/com/receitaquedoimenos/ReceitaQueDoiMenos/infra/configs/CorsConfiguration.java
+++ src/main/java/br/com/receitaquedoimenos/ReceitaQueDoiMenos/infra/configs/CorsConfiguration.java
@@ -1,32 +1,33 @@
-package br.com.receitaquedoimenos.ReceitaQueDoiMenos.infra.configs;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * Configuração para lidar com Cross-Origin Resource Sharing (CORS) na aplicação.
- */
-@Configuration
-public class CorsConfiguration {
-
-    /**
-     * Configuração CORS para permitir solicitações de origens específicas e métodos HTTP.
-     *
-     * @return Um {@link WebMvcConfigurer} com a configuração CORS.
-     */
-    @Bean
-    public WebMvcConfigurer corsConfigs() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(
-                        "*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            }
-        };
-    }
-
-}
+//package br.com.receitaquedoimenos.ReceitaQueDoiMenos.infra.configs;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * Configuração para lidar com Cross-Origin Resource Sharing (CORS) na aplicação.
+// */
+//@Configuration
+//public class CorsConfiguration {
+//
+//    /**
+//     * Configuração CORS para permitir solicitações de origens específicas e métodos HTTP.
+//     *
+//     * @return Um {@link WebMvcConfigurer} com a configuração CORS.
+//     */
+//    @Bean
+//    public WebMvcConfigurer corsConfigs() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedOrigins(
+//                        "*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//
+//}
